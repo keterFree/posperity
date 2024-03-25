4b9b8eb79fa2cfd6f49e04074481749aeda0de2a
ketertitus <ktitus@kabarak.ac.ke>
2024-03-25 20:13:30 +0300

    session

--- processData.php
+++ processData.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $_SESSION['mname'] = $_GET['mname'];
     $_SESSION['merid'] = $_GET['merid'];
     $_SESSION['suid'] = $_GET['suid'];
-
+    echo "<script>alert('setting session: " . $_SESSION['merid'] . "');</script>";
     // Set PHP variables with received values
 
     // Now you can use $php_usn, $php_mname, $php_merid, $php_suid as needed in your PHP script
@@ -18,4 +18,6 @@ $php_usn = $_SESSION['usn'];
 $php_mname = $_SESSION['mname'];
 $php_merid = $_SESSION['merid'] ;
 $php_suid = $_SESSION['suid'] ;
+echo "<script>alert('check session: " . $_SESSION['merid'] . "');</script>";
+echo "<script>alert('var value" . $php_merid . "');</script>";
 ?>
